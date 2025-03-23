3beb0921e3d5c7bc668123ce8ffe5af84111d349
BHC-Chicken <tij05078@gmail.com>
2025-03-23 20:48:31 +0900

    import 변경

--- src/main/java/com/example/quiz/entity/Room.java
+++ src/main/java/com/example/quiz/entity/Room.java
@@ -1,7 +1,10 @@
 package com.example.quiz.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 
 @Entity
