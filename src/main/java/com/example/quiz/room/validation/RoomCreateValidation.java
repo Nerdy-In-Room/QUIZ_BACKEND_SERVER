1e6dc64ccdd2bd3fb64e2c39f51a5ae9317058f1
BHC-Chicken <tij05078@gmail.com>
2025-04-06 00:31:43 +0900

    room create validation 변경

--- src/main/java/com/example/quiz/room/validation/RoomCreateValidation.java
+++ src/main/java/com/example/quiz/room/validation/RoomCreateValidation.java
@@ -4,8 +4,8 @@ import com.example.quiz.global.exception.general.GeneralErrorCode;
 import com.example.quiz.global.exception.general.GeneralErrorException;
 
 public class RoomCreateValidation {
-    public static void validateTopicId(long topicId) {
-        if (topicId <= 0 || topicId > 4) {
+    public static void validateTopicId(Long topicId) {
+        if (topicId == null || topicId <= 0 || topicId > 4) {
             throw new GeneralErrorException(GeneralErrorCode.INVALID_INPUT, "topic Id는 1이상 4이하 입니다.");
         }
     }
@@ -16,14 +16,14 @@ public class RoomCreateValidation {
         }
     }
 
-    public static void validateMaxPeople(int maxPeople) {
-        if (maxPeople < 1 || maxPeople > 8) {
+    public static void validateMaxPeople(Integer maxPeople) {
+        if (maxPeople == null || maxPeople < 1 || maxPeople > 8) {
             throw new GeneralErrorException(GeneralErrorCode.INVALID_INPUT, "최대 인원은 8명입니다.");
         }
     }
 
-    public static void validateQuizCount(int quizCount) {
-        if (quizCount < 1 || quizCount > 10) {
+    public static void validateQuizCount(Integer quizCount) {
+        if (quizCount == null || quizCount < 1 || quizCount > 10) {
             throw new GeneralErrorException(GeneralErrorCode.INVALID_INPUT, "최대 문제수는 10문제입니다.");
         }
     }
