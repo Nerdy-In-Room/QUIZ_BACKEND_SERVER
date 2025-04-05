2eff1edb0491163c90a78dc347641ce46f515305
BHC-Chicken <tij05078@gmail.com>
2025-04-06 00:23:05 +0900

    에러 추가
    - room
      - 인원 변경 현재인원 보다 작게 수정할려는 경우
      - 방 수정에 실패한 경우
    - game
      - 게임을 찾을 수 없는 경우

--- src/main/java/com/example/quiz/room/exception/RoomErrorCode.java
+++ src/main/java/com/example/quiz/room/exception/RoomErrorCode.java
@@ -9,8 +9,10 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum RoomErrorCode implements ErrorCode {
     ALREADY_IN_ANOTHER_ROOM(HttpStatus.BAD_REQUEST, "이미 다른방에 입장중입니다."),
-    MAX_ROOM(HttpStatus.BAD_REQUEST, "방이 가득 찼습니다"),
-    NOT_FOUND_ROOM(HttpStatus.BAD_REQUEST, "존재하지 않는 방입니다"),
+    MAX_ROOM(HttpStatus.BAD_REQUEST, "정원이 초과되었습니다."),
+    NOT_FOUND_ROOM(HttpStatus.BAD_REQUEST, "존재하지 않는 방입니다."),
+    WRONG_MAX_PEOPLE(HttpStatus.BAD_REQUEST, "최대인원은 현재인원보다 작을 수 없습니다."),
+    FAIL_MODIFY_ROOM(HttpStatus.FORBIDDEN, "방 수정에 실패했습니다."),
     FAIL_ENTER_ROOM(HttpStatus.INTERNAL_SERVER_ERROR, "방 입장에 실패했습니다.");
 
     private final HttpStatus status;
